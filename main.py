5b04ae2e01f3e603c35e4db44322b40682d118bd
Robert Hyman <brt@Roberts-Air.attlocal.net>
2023-04-30 04:01:18 -0400

    Model upgrade and refactor

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 import openai # gpt-3.5-turbo-0301
 import re
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI # was used for old known good but expensive model
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.chains import SequentialChain
@@ -21,8 +22,10 @@ from langchain.callbacks import get_openai_callback
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY
 
+# llm = OpenAI(temperature=.7) # old known good but expensive model
+llm = ChatOpenAI(model="gpt-3.5-turbo",temperature=.7)
+
 # This is an LLMChain to create a title given a scentence/topic.
-llm = OpenAI(temperature=.7)
 template = """You are a creative picture book writer. Given a sentence/topic, it is your job to create a title suitable for a picture book.
 
 Sentence/topic: {user_input}
@@ -31,7 +34,6 @@ prompt_template = PromptTemplate(input_variables=["user_input"], template=templa
 title_chain = LLMChain(llm=llm, prompt=prompt_template, output_key="title")
 
 # This is an LLMChain to write an synopsis of a picture book given a title and user input.
-llm = OpenAI(temperature=.7)
 template = """You are a creative picture book writer. Given the title of the 20 page picture book and the sentence/topic on which it's title is based, it is your job to write a synopsis for the picture book.
 
 Title: {title}
@@ -42,7 +44,6 @@ synopsis_chain = LLMChain(llm=llm, prompt=prompt_template, output_key="synopsis"
 
 
 # This is an LLMChain to write the text descriptions of a picture book given title and synopsis.
-llm = OpenAI(temperature=.7)
 template = """You are a creative picture book writer. Given the title and synopsis of the 20 page picture book, it is your job to write the text that should appear on each of the 20 pages.
 
 Title: {title}
@@ -53,7 +54,6 @@ prompt_template = PromptTemplate(input_variables=["title","synopsis"], template=
 text_description_chain = LLMChain(llm=llm, prompt=prompt_template, output_key="text_description")
 
 # This is an LLMChain to write the image descriptions of a picture book given title and synopsis.
-llm = OpenAI(temperature=.7)
 template = """You are an expert at creating input prompts for text-to-image neural networks. The system acepts as correct the query string,where all arguments are separated by commas. The words in prompt are crucial. Users need to prompt what they want to see, specifying artist names, media sources, or art styles to get desired results. It is more sensitive to precise wording. That includes adjectives and prepositions like “in front of [x]“, and “taken by [camera name]“. It also supports weights. By bracketing the words you can change their importance. For example, (rainy) would be twice as important compared to "rainy" for the model, and [rainy] would be half as important. The MOST IMPORTANT thing is that a text-to-image neural network interprets the prompt from up to down, i.e. what is listed at the beginning of the prompt is more significant than what is listed near the end of the prompt. So it is recommended to place the subject of prompt in the beginning, characteristical tags in the middle and misc tags like lighting or camera settings near the end. Tags must be separated by commas, commas are not allowed in the query (what needs to be drawn), because the system treats it as one big tag. Given the title and text for each page, it is your job to write the prompt for the image that should accompany the text on each page.
 
 Title: {title}
@@ -68,7 +68,6 @@ with get_openai_callback() as cb:
     overall_chain = SequentialChain(
         chains=[title_chain, synopsis_chain, text_description_chain, image_description_chain],
         input_variables=["user_input"],
-        # Here we return multiple variables
         output_variables=["title","synopsis","text_description","image_description"],
         verbose=True)
     print(f"Total Tokens: {cb.total_tokens}")
